3c65ba37c34c56042d82552dd4e39d36844415ec
MarcosRubi <danielhernandez9980@gmail.com>
2022-10-18 11:46:21 -0600

    Cors habilitado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -11,6 +12,7 @@ app.set("json spaces", 2);
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 
 //Routes
 app.get("/game/:game", Game.get);
